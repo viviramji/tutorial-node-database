f458f69a75ea15b5972b3e59647a2f16cdffc17a
viviramji <wrjr04@gmail.com>
2018-10-30 19:45:20 -0500

    Setup knex

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        user: 'vramos',
+        password: 'password',
+        database: 'tutorial_node_database'
+    }
+}
---
+++ migrations/20181030190524_create_user_table.js
@@ -0,0 +1,13 @@
+//create
+
+exports.up = function (knex) {
+    return knex.schema.createTable('user', function (t) {
+      t.increments('id').primary()
+      t.string('username').notNullable()
+      t.string('password').notNullable()
+      t.timestamps(false, true)
+    })
+  }
+exports.down = function (knex) {
+return knex.schema.dropTableIfExists('user')
+}
